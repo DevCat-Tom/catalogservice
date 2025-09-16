32d4b9c7cf497bfb640c66c1a4ddebadfb8ad174
DevCat <you@example.com>
2025-09-16 09:58:15 +0200

    - Setup project - Add HomeController

---
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.catalogservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    String getGreeting() {
+        return "Welcome to Spring Boot Catalog Service!";
+    }
+}
