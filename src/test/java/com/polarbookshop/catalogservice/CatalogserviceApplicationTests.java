d6a80fcd321f2a8bc0c98bba51e0917d6544963b
DevCat <you@example.com>
2025-09-18 15:29:17 +0200

    CHAPTER 5: - Config Testcontainers for PostgreSQL (dependency + profile) - Add JDBC test using Testcontainer

--- src/test/java/com/polarbookshop/catalogservice/CatalogserviceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogserviceApplicationTests.java
@@ -4,19 +4,19 @@ import com.polarbookshop.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /*
-* req:
-* testImplementation 'org.springframework.boot:spring-boot-starter-webflux'
-*
-* ./gradlew test --tests CatalogserviceApplicationTests
-* */
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
+ * req:
+ * testImplementation 'org.springframework.boot:spring-boot-starter-webflux'
+ *
+ * ./gradlew test --tests CatalogserviceApplicationTests
+ * */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("integration")
 class CatalogserviceApplicationTests {
     @Autowired
     private WebTestClient webTestClient;
