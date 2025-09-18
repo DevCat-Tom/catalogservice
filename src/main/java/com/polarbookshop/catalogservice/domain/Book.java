717b1508ee2488ce955a9dad115cc7130381804d
DevCat <you@example.com>
2025-09-18 15:11:20 +0200

    CHAPTER 5: - Running PostgreSQL as a container - Add dependencies and config JDBC connection - Defining identifier and version for Books - Enable and config JDBC auditing - Support CRUD using CrudRepository interface

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -4,8 +4,23 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
 
+import java.time.Instant;
+
+/*
+NOTE: Spring Data JPA works with mutating objects, so you can’t use Java
+records. JPA entity classes must be marked with the @Entity annotation and
+expose a no-args constructor. JPA identifiers are annotated with @Id and
+@Version from the javax.persistence package instead of org.spring-
+framework.data.annotation.
+* */
 public record Book(
+        @Id
+        Long id,
         @NotBlank(message = "The book ISBN must be defined.")
         @Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "The ISBN format must be valid.")
         String isbn,
@@ -15,6 +30,19 @@ public record Book(
         String author,
         @NotNull(message = "The book price must be defined.")
         @Positive(message = "The book price must be greater than zero.")
-        Double price
+        Double price,
+        @CreatedDate
+        Instant createdDate,
+        @LastModifiedDate
+        Instant lastModifiedDate,
+        @Version
+        int version
 ) {
+
+    public static Book of(String isbn, String title, String author, Double price) {
+        // An entity is considered new when the ID is null and the version is 0.
+        return new Book(null, isbn, title, author, price, null, null, 0);
+    }
 }
+
+
